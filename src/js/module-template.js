43ed2be0225bcc129bb785dec1c230296741cd38
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-10-07 16:24:59 +0900

    add a template for node-module

---
+++ src/js/module-template.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * hogehoge // Intro for this app
+ *
+ * * Dependencies // list up dependecies here
+ * - jQuery 3.4.1
+ *
+ */
+
+import jQuery from 'jquery';
+
+const HogeEve = (function(w, d, $) {
+  // The app`s capital letter must be uppercase, then add 'Eve' after the name
+  function hoge() {}
+
+  hoge.prototype = {
+    constructor: hoge
+  };
+
+  return hoge; // return hoge() and insert into const HogeEve
+})(window, document, jQuery); // Each item is for scope. w = window, d = document, and $ = jQuery
+
+export default HogeEve; // export the above object to wherever it is imported. This will make the app a module
